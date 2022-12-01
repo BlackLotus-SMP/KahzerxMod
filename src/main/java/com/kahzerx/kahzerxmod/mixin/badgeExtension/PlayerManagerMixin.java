0cd818d9b5c78c73bfb6877b7f6c0f3035870b2a
kahzerx <kahzerx@tutanota.com>
2022-12-01 23:45:02 +0100

    badge fix

--- src/main/java/com/kahzerx/kahzerxmod/mixin/badgeExtension/PlayerManagerMixin.java
+++ src/main/java/com/kahzerx/kahzerxmod/mixin/badgeExtension/PlayerManagerMixin.java
@@ -26,34 +26,36 @@ public class PlayerManagerMixin {
     private void onBroadcast(ServerPlayerEntity instance, SentMessage message, boolean bl, MessageType.Parameters parameters, SignedMessage signedMessage, Predicate<ServerPlayerEntity> shouldSendFiltered, @Nullable ServerPlayerEntity sender, MessageSourceProfile messageSourceProfile, MessageType.Parameters parameters2) {
         if (BadgeExtension.isExtensionEnabled && sender != null) {
             MutableText t = Text.literal("");
-            if (BadgeExtension.playerBadges.size() != 0) {
-                List<BadgeInstance> badges = BadgeExtension.playerBadges.get(sender.getUuid().toString());
-                if (badges.size() != 0) {
-                    t = Text.literal(" ");
-                    MutableText hover = Text.literal("");
-                    hover.append(Text.literal("Badges:").styled(style -> style.withColor(Formatting.WHITE)));
-                    for (BadgeInstance badgeInstance : badges) {
-                        hover.append(Text.literal("\n" + badgeInstance.getBadge()).styled(style -> style.withColor(Formatting.byColorIndex(badgeInstance.getColorIndex()))));
-                        hover.append(Text.literal(String.format(": %s", badgeInstance.getDescription())).styled(style -> style.withColor(Formatting.WHITE)));
-                    }
-                    MutableText bgs = Text.literal("");
-                    for (int i = badges.size() - 1; i >= 0; i--) {
-                        BadgeInstance badge = badges.get(i);
-                        bgs.append(Text.literal(badge.getBadge()).styled(style -> style.withColor(Formatting.byColorIndex(badge.getColorIndex()))));
-                        bgs.append(Text.literal("").styled(style -> style.withColor(Formatting.WHITE)));
-                        if (i < badges.size() - 2 || i == 0) {
-                            break;
-                        }
-                        bgs.append(Text.literal(" "));
-                    }
-                    t.append(bgs.styled(style -> style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hover))));
+            List<BadgeInstance> badges = BadgeExtension.playerBadges.get(sender.getUuid().toString());
+            if (badges.size() != 0) {
+                t = Text.literal(" ");
+                MutableText hover = Text.literal("");
+                hover.append(Text.literal("Badges:").styled(style -> style.withColor(Formatting.WHITE)));
+                for (BadgeInstance badgeInstance : badges) {
+                    hover.append(Text.literal("\n" + badgeInstance.getBadge()).styled(style -> style.withColor(Formatting.byColorIndex(badgeInstance.getColorIndex()))));
+                    hover.append(Text.literal(String.format(": %s", badgeInstance.getDescription())).styled(style -> style.withColor(Formatting.WHITE)));
                 }
+                MutableText bgs = Text.literal("");
+                for (int i = badges.size() - 1; i >= 0; i--) {
+                    BadgeInstance badge = badges.get(i);
+                    bgs.append(Text.literal(badge.getBadge()).styled(style -> style.withColor(Formatting.byColorIndex(badge.getColorIndex()))));
+                    bgs.append(Text.literal("").styled(style -> style.withColor(Formatting.WHITE)));
+                    if (i < badges.size() - 2 || i == 0) {
+                        break;
+                    }
+                    bgs.append(Text.literal(" "));
+                }
+                t.append(bgs.styled(style -> style.withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, hover))));
             }
             MutableText display = MutableText.of(sender.getDisplayName().getContent());
             display.setStyle(sender.getDisplayName().getStyle());
-            display.append(sender.getDisplayName().getSiblings().get(0));
-            display.append(sender.getDisplayName().getSiblings().get(1));
-            MutableText m = sender.getDisplayName().getSiblings().get(2).copy();
+            System.out.println(sender.getDisplayName().getSiblings());
+            MutableText m = Text.literal("");
+            if (sender.getDisplayName().getSiblings().size() != 0) {
+                display.append(sender.getDisplayName().getSiblings().get(0));
+                display.append(sender.getDisplayName().getSiblings().get(1));
+                m.append(sender.getDisplayName().getSiblings().get(2).copy());
+            }
             m.append(t);
             display.append(m);
             MessageType.Parameters params = MessageType.params(MessageType.CHAT, sender.world.getRegistryManager(), display);
