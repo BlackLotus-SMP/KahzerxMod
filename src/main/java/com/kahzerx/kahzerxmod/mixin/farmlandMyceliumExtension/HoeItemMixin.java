799a3c5777fda6c75dcdd88b7992e1eff76b9505
kahzerx <kahzerx@tutanota.com>
2022-12-01 17:47:09 +0100

    block swap, no sound, no swing

--- src/main/java/com/kahzerx/kahzerxmod/mixin/farmlandMyceliumExtension/HoeItemMixin.java
+++ src/main/java/com/kahzerx/kahzerxmod/mixin/farmlandMyceliumExtension/HoeItemMixin.java
@@ -1,47 +1,23 @@
 package com.kahzerx.kahzerxmod.mixin.farmlandMyceliumExtension;
 
-import com.kahzerx.kahzerxmod.extensions.farmlandMyceliumExtension.FarmlandMyceliumExtension;
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
-import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.HoeItem;
-import net.minecraft.item.ItemUsageContext;
-import net.minecraft.sound.SoundCategory;
-import net.minecraft.sound.SoundEvents;
-import net.minecraft.util.ActionResult;
+import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(HoeItem.class)
 public class HoeItemMixin {
-    private boolean replaced = false;
-
-    @Inject(method = "useOnBlock", at = @At(value = "HEAD"))
-    private void onUse(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
-        World world = context.getWorld();
-        Block b = world.getBlockState(context.getBlockPos()).getBlock();
-        if (b == Blocks.MYCELIUM && FarmlandMyceliumExtension.isExtensionEnabled) {
-            world.setBlockState(context.getBlockPos(), Blocks.GRASS_BLOCK.getDefaultState());
-            world.updateNeighbors(context.getBlockPos(), Blocks.GRASS_BLOCK);
-            replaced = true;
-            PlayerEntity player = context.getPlayer();
-            if (player != null) {
-                player.playSound(SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0f, 1.0f);
-            }
+    @Redirect(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getBlockState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/BlockState;"))
+    private BlockState onGetBlock(World instance, BlockPos pos) {
+        Block b = instance.getBlockState(pos).getBlock();
+        if (b == Blocks.MYCELIUM) {
+            return Blocks.DIRT.getDefaultState();
         }
-    }
-
-    @Redirect(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/ActionResult;success(Z)Lnet/minecraft/util/ActionResult;"))
-    private ActionResult onSuccess(boolean swingHand, ItemUsageContext context) {
-        World world = context.getWorld();
-        if (replaced) {
-            replaced = false;
-            return ActionResult.success(true);
-        }
-        return ActionResult.success(world.isClient);
+        return instance.getBlockState(pos);
     }
 }
