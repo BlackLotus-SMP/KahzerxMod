2c676c0fd774a3167c9ef2d947fa9c24e4e13f00
kahzerx <kahzerx@tutanota.com>
2022-12-01 18:27:50 +0100

    sound and swing fix

--- src/main/java/com/kahzerx/kahzerxmod/mixin/farmlandMyceliumExtension/HoeItemMixin.java
+++ src/main/java/com/kahzerx/kahzerxmod/mixin/farmlandMyceliumExtension/HoeItemMixin.java
@@ -4,20 +4,43 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.item.HoeItem;
+import net.minecraft.item.ItemUsageContext;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.util.ActionResult;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(HoeItem.class)
 public class HoeItemMixin {
+    private boolean replaced = false;
     @Redirect(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;getBlockState(Lnet/minecraft/util/math/BlockPos;)Lnet/minecraft/block/BlockState;"))
     private BlockState onGetBlock(World instance, BlockPos pos) {
         Block b = instance.getBlockState(pos).getBlock();
         if (b == Blocks.MYCELIUM) {
+            replaced = true;
             return Blocks.DIRT.getDefaultState();
         }
         return instance.getBlockState(pos);
+    }
+
+    @Inject(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/World;playSound(Lnet/minecraft/entity/player/PlayerEntity;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/sound/SoundEvent;Lnet/minecraft/sound/SoundCategory;FF)V"))
+    private void justPlay(ItemUsageContext context, CallbackInfoReturnable<ActionResult> cir) {
+        if (this.replaced) {
+            context.getWorld().playSound(null, context.getBlockPos(), SoundEvents.ITEM_HOE_TILL, SoundCategory.BLOCKS, 1.0f, 1.0f);
+        }
+    }
+
+    @Redirect(method = "useOnBlock", at = @At(value = "INVOKE", target = "Lnet/minecraft/util/ActionResult;success(Z)Lnet/minecraft/util/ActionResult;"))
+    private ActionResult onAccept(boolean swingHand) {
+        if (this.replaced) {
+            return ActionResult.SUCCESS;
+        }
+        return ActionResult.success(swingHand);
     }
 }
