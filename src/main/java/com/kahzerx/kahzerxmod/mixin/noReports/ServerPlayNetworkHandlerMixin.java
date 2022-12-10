41899ffd872fdeab4caccf64e243adb6984b95bc
kahzerx <kahzerx@tutanota.com>
2022-12-10 14:43:15 +0100

    no reports fix

--- src/main/java/com/kahzerx/kahzerxmod/mixin/noReports/ServerPlayNetworkHandlerMixin.java
+++ src/main/java/com/kahzerx/kahzerxmod/mixin/noReports/ServerPlayNetworkHandlerMixin.java
@@ -4,7 +4,6 @@ import net.minecraft.network.Packet;
 import net.minecraft.network.PacketCallbacks;
 import net.minecraft.network.packet.s2c.play.ChatMessageS2CPacket;
 import net.minecraft.network.packet.s2c.play.GameMessageS2CPacket;
-import net.minecraft.network.packet.s2c.play.MessageHeaderS2CPacket;
 import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
@@ -21,10 +20,8 @@ public abstract class ServerPlayNetworkHandlerMixin {
 
     @Inject(method = "sendPacket(Lnet/minecraft/network/Packet;Lnet/minecraft/network/PacketCallbacks;)V", at = @At(value = "HEAD"), cancellable = true)
     private void onSend(Packet<?> packet, PacketCallbacks callbacks, CallbackInfo ci) {
-        if (packet instanceof MessageHeaderS2CPacket) {
-            ci.cancel();
-        } else if (packet instanceof ChatMessageS2CPacket chatPacket) {
-            sendPacket(new GameMessageS2CPacket(chatPacket.serializedParameters().toParameters(this.player.world.getRegistryManager()).get().applyChatDecoration(chatPacket.message().getContent()), false));
+        if (packet instanceof ChatMessageS2CPacket chatPacket) {
+            sendPacket(new GameMessageS2CPacket(chatPacket.serializedParameters().toParameters(this.player.world.getRegistryManager()).get().applyChatDecoration(chatPacket.unsignedContent()), false));
             ci.cancel();
         }
     }
