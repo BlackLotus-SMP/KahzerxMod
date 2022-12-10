721b56551e562a9a74d4edb3558d7f97677a0379
kahzerx <kahzerx@tutanota.com>
2022-12-10 17:05:18 +0100

    fbi extension fix

--- src/main/java/com/kahzerx/kahzerxmod/mixin/fbiExtension/PlayerManagerMixin.java
+++ src/main/java/com/kahzerx/kahzerxmod/mixin/fbiExtension/PlayerManagerMixin.java
@@ -2,25 +2,29 @@ package com.kahzerx.kahzerxmod.mixin.fbiExtension;
 
 
 import com.mojang.authlib.GameProfile;
-import net.minecraft.network.Packet;
 import net.minecraft.network.packet.s2c.play.PlayerListS2CPacket;
 import net.minecraft.server.PlayerManager;
-import net.minecraft.server.network.ServerPlayNetworkHandler;
 import net.minecraft.server.network.ServerPlayerEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
+import java.util.ArrayList;
+import java.util.Collection;
+
 import static com.kahzerx.kahzerxmod.extensions.fbiExtension.FBIExtension.getHiddenPlayers;
 
 @Mixin(PlayerManager.class)
 public class PlayerManagerMixin {
-    @Redirect(method = "onPlayerConnect", at = @At(value = "INVOKE", target = "Lnet/minecraft/server/network/ServerPlayNetworkHandler;sendPacket(Lnet/minecraft/network/Packet;)V", ordinal = 7))
-    public void onConnect(ServerPlayNetworkHandler instance, Packet<?> packet) {
-        PlayerListS2CPacket p = (PlayerListS2CPacket) packet;
-        if (!isHidden(p.getEntries().get(0).profile())) {
-            instance.sendPacket(packet);
+    @Redirect(method = "onPlayerConnect", at = @At(value = "INVOKE", target = "Lnet/minecraft/network/packet/s2c/play/PlayerListS2CPacket;entryFromPlayer(Ljava/util/Collection;)Lnet/minecraft/network/packet/s2c/play/PlayerListS2CPacket;"))
+    public PlayerListS2CPacket onConnect(Collection<ServerPlayerEntity> players) {
+        Collection<ServerPlayerEntity> players_ = new ArrayList<>();
+        for (ServerPlayerEntity player : players) {
+            if (!isHidden(player.getGameProfile())) {
+                players_.add(player);
+            }
         }
+        return PlayerListS2CPacket.entryFromPlayer(players_);
     }
 
     private boolean isHidden(GameProfile profile) {
