c7c8414d9c6edfd88571e6056d51588424838f6c
kahzerx <kahzerx@tutanota.com>
2022-12-10 14:22:43 +0100

    create player fix

--- src/main/java/com/kahzerx/kahzerxmod/mixin/kloneExtension/PlayerManagerMixin.java
+++ src/main/java/com/kahzerx/kahzerxmod/mixin/kloneExtension/PlayerManagerMixin.java
@@ -2,11 +2,8 @@ package com.kahzerx.kahzerxmod.mixin.kloneExtension;
 
 import com.kahzerx.kahzerxmod.klone.KlonePlayerEntity;
 import com.mojang.authlib.GameProfile;
-import net.minecraft.network.encryption.PlayerPublicKey;
-import net.minecraft.server.MinecraftServer;
 import net.minecraft.server.PlayerManager;
 import net.minecraft.server.network.ServerPlayerEntity;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -20,7 +17,7 @@ public abstract class PlayerManagerMixin {
     @Shadow public abstract List<ServerPlayerEntity> getPlayerList();
 
     @Inject(method = "createPlayer", at = @At("HEAD"))
-    private void onCreatePlayer(GameProfile profile, PlayerPublicKey publicKey, CallbackInfoReturnable<ServerPlayerEntity> cir) {
+    private void onCreatePlayer(GameProfile profile, CallbackInfoReturnable<ServerPlayerEntity> cir) {
         for (ServerPlayerEntity player : getPlayerList()) {
             if (player.getGameProfile().getId().equals(profile.getId()) && player.getClass() == KlonePlayerEntity.class) {
                  player.kill();
