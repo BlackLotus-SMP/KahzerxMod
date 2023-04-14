dda1f5249ff4cfbf92eb917490a2817ea16c6d89
Sol Robledo <anasolrobledo@gmail.com>
2023-04-14 20:50:44 -0300

    info command fup :D

---
+++ src/main/java/com/kahzerx/kahzerxmod/extensions/discordExtension/commands/InfoCommand.java
@@ -0,0 +1,162 @@
+package com.kahzerx.kahzerxmod.extensions.discordExtension.commands;
+
+import com.kahzerx.kahzerxmod.extensions.discordExtension.DiscordPermission;
+import com.kahzerx.kahzerxmod.extensions.discordExtension.discordWhitelistExtension.DiscordWhitelistExtension;
+import com.kahzerx.kahzerxmod.extensions.discordExtension.utils.DiscordChatUtils;
+import com.mojang.authlib.GameProfile;
+import net.dv8tion.jda.api.EmbedBuilder;
+import net.dv8tion.jda.api.entities.Guild;
+import net.dv8tion.jda.api.entities.Member;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.requests.RestAction;
+import net.minecraft.client.realms.FileUpload;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.WorldSavePath;
+
+import java.awt.*;
+import java.io.*;
+import java.net.URL;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.Optional;
+import java.util.UUID;
+
+public class InfoCommand extends GenericCommand {
+
+    public InfoCommand(String prefix) {
+        super("info", DiscordPermission.WHITELIST_CHAT, prefix + "info <playerName> o " + prefix + "info");
+    }
+
+    public void execute(MessageReceivedEvent event, MinecraftServer server, String serverPrefix, DiscordWhitelistExtension extension){
+        String destinationFile = "skin.png";
+        boolean feedback = extension.getDiscordExtension().extensionSettings().isShouldFeedback();
+        String msg = event.getMessage().getContentRaw();
+        if (msg.split(" ").length == 2) {
+            String playerName = msg.split(" ")[1];
+            Optional<GameProfile> profile = server.getUserCache().findByName(playerName);
+            if (profile.isEmpty()) {
+                EmbedBuilder embed = DiscordChatUtils.generateEmbed(new String[]{"**Not premium.**"}, serverPrefix, true, Color.RED, true, feedback);
+                if (embed != null) {
+                    event.getChannel().sendMessageEmbeds(embed.build()).queue();
+                }
+                return;
+            }
+            UUID playerUuid = profile.get().getId();
+            if (extension.alreadyAddedBySomeone(playerUuid)) {
+                try {
+                    File skinPng = saveImage(playerUuid.toString(), server, serverPrefix, event, feedback);
+                    PlayerData embeDitto = collectData(skinPng, playerUuid.toString(), extension, event.getGuild());
+                    EmbedBuilder embedFinal = new EmbedBuilder();
+                    embedFinal.setTitle(embeDitto.mcNick);
+                    embedFinal.setColor(new Color(0xABDED7));
+                    embedFinal.setDescription(String.format("Added by: %s", embeDitto.dsNick));
+                    embedFinal.setThumbnail("attachment://skin.png");
+                    embedFinal.addField("Uuid: ", embeDitto.playerUuid, true);
+                    event.getChannel().sendMessageEmbeds(embedFinal.build()).addFile(embeDitto.skinPath, "skin.png").queue();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    EmbedBuilder embed = DiscordChatUtils.generateEmbed(new String[]{"**There has been an error**"}, serverPrefix, true, Color.RED, true, feedback);
+                    if (embed != null) {
+                        event.getChannel().sendMessageEmbeds(embed.build()).queue();
+                    }
+                }
+            }else {
+                EmbedBuilder embed = DiscordChatUtils.generateEmbed(new String[]{"**Not in whitelist.**"}, serverPrefix, true, Color.RED, true, feedback);
+                if (embed != null) {
+                    event.getChannel().sendMessageEmbeds(embed.build()).queue();
+                }
+                return;
+            }
+        }else {
+            long dsId = event.getAuthor().getIdLong();
+            ArrayList<String> whiteList = extension.getWhitelistedPlayers(dsId);
+            for (String uuid: whiteList){
+                try {
+                    File skinPng = saveImage(uuid, server, serverPrefix, event, feedback);
+                    PlayerData embeDitto = collectData(skinPng, uuid, extension, event.getGuild());
+                    EmbedBuilder embedFinal = new EmbedBuilder();
+                    embedFinal.setTitle(embeDitto.mcNick);
+                    embedFinal.setColor(new Color(0xABDED7));
+                    embedFinal.setDescription(String.format("Added by: %s", embeDitto.dsNick));
+                    embedFinal.setThumbnail("attachment://skin.png");
+                    embedFinal.addField("Uuid: ", embeDitto.playerUuid, true);
+                    event.getChannel().sendMessageEmbeds(embedFinal.build()).addFile(embeDitto.skinPath, "skin.png").queue();
+
+                } catch (IOException e) {
+                    e.printStackTrace();
+                    EmbedBuilder embed = DiscordChatUtils.generateEmbed(new String[]{"**There has been an error**"}, serverPrefix, true, Color.RED, true, feedback);
+                    if (embed != null) {
+                        event.getChannel().sendMessageEmbeds(embed.build()).queue();
+                    }
+                }
+            }
+
+        }
+
+    }
+
+    public File saveImage(String uuid, MinecraftServer server, String serverPrefix, MessageReceivedEvent event, Boolean feedback) throws IOException {
+        URL url = new URL(String.format("https://crafatar.com/renders/body/%s?overlay", uuid));
+        Path worldPath = server.getSavePath(WorldSavePath.ROOT);
+        File finalPath = new File(worldPath.toFile().getAbsolutePath() + String.format("skins/%s.png", uuid));
+        File directoryPath = finalPath.getParentFile();
+        if (!directoryPath.exists()){
+            Files.createDirectories(directoryPath.toPath());
+        }
+        File[] skins = directoryPath.listFiles();
+        if (skins == null) {
+            EmbedBuilder embed = DiscordChatUtils.generateEmbed(new String[]{"**Could not list the files :c**"}, serverPrefix, true, Color.RED, true, feedback);
+            if (embed != null) {
+                event.getChannel().sendMessageEmbeds(embed.build()).queue();
+            }
+            return null;
+        }
+        boolean cuteFlag = false;
+        for (File skin: skins) {
+            String fileName = skin.getName();
+            if (fileName.equals(String.format("%s.png", uuid))){
+                cuteFlag = true;
+                break;
+            }
+        }
+        if (!cuteFlag){
+            InputStream input = url.openStream();
+            OutputStream output = new FileOutputStream(finalPath);
+
+            byte[] b = new byte[2048];
+            int length;
+
+            while ((length = input.read(b)) != -1) {
+                output.write(b, 0, length);
+            }
+
+            input.close();
+            output.close();
+        }
+        return finalPath;
+    }
+    public PlayerData collectData(File skinPng, String uuid, DiscordWhitelistExtension extension, Guild guild){
+        Long dsId = extension.getDiscordID(uuid);
+        Member dsName = guild.retrieveMemberById(dsId).complete();
+        String mcName = extension.getMinecraftNick(uuid);
+        return new PlayerData(skinPng, mcName, dsName.getEffectiveName(),uuid);
+    }
+
+    class PlayerData{
+        public File skinPath;
+        public String mcNick;
+        public String dsNick;
+        public String playerUuid;
+//        public String status;
+//        public String serverRole;
+        public PlayerData(File skinPath, String mcNick, String dsNick, String playerUuid) {
+            this.skinPath = skinPath;
+            this.mcNick = mcNick;
+            this.dsNick = dsNick;
+            this.playerUuid = playerUuid;
+//            this.status = status;
+//            this.serverRole = serverRole;
+        }
+    }
+}
