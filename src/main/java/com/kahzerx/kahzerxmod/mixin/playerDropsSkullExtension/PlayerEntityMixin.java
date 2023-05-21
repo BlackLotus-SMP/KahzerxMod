d89b87fdb578fe95bc9b4f22332ecef5b080b065
kahzerx <kahzerx@tutanota.com>
2023-05-21 13:52:57 +0200

    drop skull fix

--- src/main/java/com/kahzerx/kahzerxmod/mixin/playerDropsSkullExtension/PlayerEntityMixin.java
+++ src/main/java/com/kahzerx/kahzerxmod/mixin/playerDropsSkullExtension/PlayerEntityMixin.java
@@ -3,18 +3,18 @@ package com.kahzerx.kahzerxmod.mixin.playerDropsSkullExtension;
 import com.kahzerx.kahzerxmod.extensions.playerDropsSkullExtension.PlayerDropsSkullExtension;
 import com.mojang.authlib.GameProfile;
 import net.minecraft.entity.damage.DamageSource;
+import net.minecraft.entity.damage.DamageTypes;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtCompound;
-import net.minecraft.network.encryption.PlayerPublicKey;
 import net.minecraft.server.network.ServerPlayerEntity;
 import net.minecraft.util.ItemScatterer;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+
 
 @Mixin(ServerPlayerEntity.class)
 public abstract class PlayerEntityMixin extends PlayerEntity {
@@ -27,7 +27,7 @@ public abstract class PlayerEntityMixin extends PlayerEntity {
     @Override
     protected void dropLoot(DamageSource source, boolean causedByPlayer) {
         super.dropLoot(source, causedByPlayer);
-        if (PlayerDropsSkullExtension.isExtensionEnabled && source == DamageSource.LIGHTNING_BOLT) {
+        if (PlayerDropsSkullExtension.isExtensionEnabled && source.isOf(DamageTypes.LIGHTNING_BOLT)) {
             if ((causedByPlayer && random.nextDouble() < 0.12D) || (!causedByPlayer && random.nextDouble() < 0.30D)) {
                 ItemStack stack = new ItemStack(Items.PLAYER_HEAD);
                 NbtCompound compound = stack.getOrCreateNbt();
