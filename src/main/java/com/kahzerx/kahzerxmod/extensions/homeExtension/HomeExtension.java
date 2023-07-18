89d37e0b2be1da70aa79b7947b7afc6b47bacee3
kahzerx <kahzerx@tutanota.com>
2023-07-18 18:07:45 +0200

    home extension

--- src/main/java/com/kahzerx/kahzerxmod/extensions/homeExtension/HomeExtension.java
+++ src/main/java/com/kahzerx/kahzerxmod/extensions/homeExtension/HomeExtension.java
@@ -24,8 +24,8 @@ public class HomeExtension extends GenericExtension implements Extensions {
     private Connection conn;
     private MinecraftServer server;
 
-    public HomeExtension(ExtensionSettings settings) {
-        super(settings);
+    public HomeExtension(HashMap<String, Boolean> config) {
+        super(new ExtensionSettings(config, "home", "/home and /setHome commands."));
     }
 
     @Override
