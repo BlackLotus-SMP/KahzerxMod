66e3bddf12b1920ff68265d1a12c3ecd10525230
kahzerx <kahzerx@tutanota.com>
2023-08-17 21:34:58 +0200

    discord processor

--- src/main/java/com/kahzerx/kahzerxmod/extensions/discordExtension/discordExtension/DiscordExtension.java
+++ src/main/java/com/kahzerx/kahzerxmod/extensions/discordExtension/discordExtension/DiscordExtension.java
@@ -72,16 +72,12 @@ public class DiscordExtension extends DiscordGenericExtension implements Extensi
 
     @Override
     protected boolean processCommands(MessageReceivedEvent event, String message, MinecraftServer server) {
-        if (!this.extensionSettings().isEnabled()) {
+        CommandFound commandFound = this.findValidCommand(event, message, this.extensionSettings().getAllowedChats(), this.extensionSettings().isShouldFeedback(), this.extensionSettings().getPrefix());
+        if (!commandFound.found()) {
             return false;
         }
-        if (!DiscordUtils.isAllowed(event.getChannel().getIdLong(), this.extensionSettings().getAllowedChats())) {
-            if (message.startsWith(this.onlineCommand.getCommandPrefix() + this.onlineCommand.getCommand())) {
-                return true;
-            }
-        }
-        if (message.equals(this.onlineCommand.getCommandPrefix() + this.onlineCommand.getCommand())) {
-            this.onlineCommand.execute(event, server, this.extensionSettings().getPrefix(), this.extensionSettings().getAllowedChats());
+        if (commandFound.command() != null) {
+            commandFound.command().executeCommand(event, server, this.getEm());
             return true;
         }
         return false;
