7a50cf30c5dabae524355e5ac69297032d6da58e
kahzerx <kahzerx@tutanota.com>
2023-08-17 21:52:24 +0200

    ban command

--- src/main/java/com/kahzerx/kahzerxmod/extensions/discordExtension/commands/BanCommand.java
+++ src/main/java/com/kahzerx/kahzerxmod/extensions/discordExtension/commands/BanCommand.java
@@ -3,6 +3,7 @@ package com.kahzerx.kahzerxmod.extensions.discordExtension.commands;
 import com.kahzerx.kahzerxmod.ExtensionManager;
 import com.kahzerx.kahzerxmod.extensions.discordExtension.DiscordPermission;
 import com.kahzerx.kahzerxmod.extensions.discordExtension.discordAdminToolsExtension.DiscordAdminToolsExtension;
+import com.kahzerx.kahzerxmod.extensions.discordExtension.discordExtension.DiscordExtension;
 import com.kahzerx.kahzerxmod.extensions.discordExtension.discordWhitelistExtension.DiscordWhitelistExtension;
 import com.kahzerx.kahzerxmod.extensions.discordExtension.utils.DiscordChatUtils;
 import com.mojang.authlib.GameProfile;
@@ -22,8 +23,12 @@ public class BanCommand extends GenericCommand {
     }
 
     @Override
-    public void execute(MessageReceivedEvent event, MinecraftServer server, String serverPrefix, DiscordWhitelistExtension extension, DiscordAdminToolsExtension adminExtension) {
-        boolean feedback = adminExtension.extensionSettings().isShouldFeedback();
+    public void executeCommand(MessageReceivedEvent event, MinecraftServer server, ExtensionManager extensionManager) {
+        DiscordWhitelistExtension discordWhitelistExtension = extensionManager.getDiscordWhitelistExtension();
+        DiscordExtension discordExtension = extensionManager.getDiscordExtension();
+        DiscordAdminToolsExtension discordAdminToolsExtension = extensionManager.getDiscordAdminToolsExtension();
+        String serverPrefix = discordExtension.extensionSettings().getPrefix();
+        boolean feedback = discordAdminToolsExtension.extensionSettings().isShouldFeedback();
         String[] req = event.getMessage().getContentRaw().split(" ");
         String playerName = req[1];
         if (req.length != 2) {
@@ -39,40 +44,35 @@ public class BanCommand extends GenericCommand {
             }
             return;
         }
-        if (!extension.alreadyAddedBySomeone(profile.get().getId().toString())) {
+        if (!discordWhitelistExtension.alreadyAddedBySomeone(profile.get().getId().toString())) {
             EmbedBuilder embed = DiscordChatUtils.generateEmbed(new String[]{"**Not added.**"}, serverPrefix, true, Color.RED, true, feedback);
             if (embed != null) {
                 event.getChannel().sendMessageEmbeds(embed.build()).queue();
             }
             return;
         }
-        if (extension.canRemove(69420L, profile.get().getId().toString())) {
+        if (discordWhitelistExtension.canRemove(69420L, profile.get().getId().toString())) {
             EmbedBuilder embed = DiscordChatUtils.generateEmbed(new String[]{"**Added with !exadd... do !exremove.**"}, serverPrefix, true, Color.RED, true, feedback);
             if (embed != null) {
                 event.getChannel().sendMessageEmbeds(embed.build()).queue();
             }
             return;
         }
-        long discordID = extension.getDiscordID(profile.get().getId().toString());
-        if (extension.isPlayerBanned(profile.get().getId().toString())) {
-            onBanAction(extension, discordID, server);
+        long discordID = discordWhitelistExtension.getDiscordID(profile.get().getId().toString());
+        if (discordWhitelistExtension.isPlayerBanned(profile.get().getId().toString())) {
+            onBanAction(discordWhitelistExtension, discordID, server);
             EmbedBuilder embed = DiscordChatUtils.generateEmbed(new String[]{"**Already banned!**"}, serverPrefix, true, Color.YELLOW, true, feedback);
             if (embed != null) {
                 event.getChannel().sendMessageEmbeds(embed.build()).queue();
             }
             return;
         }
-        extension.banDiscord(discordID);
-        onBanAction(extension, discordID, server);
+        discordWhitelistExtension.banDiscord(discordID);
+        onBanAction(discordWhitelistExtension, discordID, server);
         EmbedBuilder embed = DiscordChatUtils.generateEmbed(new String[]{"**Banned! :D**"}, serverPrefix, true, Color.GREEN, true, feedback);
         if (embed != null) {
             event.getChannel().sendMessageEmbeds(embed.build()).queue();
         }
-    }
-
-    @Override
-    public void executeCommand(MessageReceivedEvent event, MinecraftServer server, ExtensionManager extensionManager) {
-
     }
 
     private void onBanAction(DiscordWhitelistExtension extension, long discordID, MinecraftServer server) {
